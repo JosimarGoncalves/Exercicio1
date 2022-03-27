d8ab2659a1a7281d3d546fdfcb836f5b8b9ad420
Josimar Goncalves <foxchanger@hotmail.com>
2022-03-27 01:30:17 -0300

    Upd Code Complete

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using System.Threading;
+using System.Diagnostics;
 
 class Program
 {
-    public static int[] numeroSorteado = new int[100];
+    public static int[] numeroSorteado = new int[200];
     public static int valorMinimo = 10;
     public static int valorMaximo = 100;
-    public static int somaA = 0;
-    public static int somaB = 0;
+    public static int soma1 = 0;
+    public static int soma2 = 0;
+    public static int somaTotal = 0;
 
     static void Main()
     {
         Random numeroAleatorio = new Random();
 
-        Console.WriteLine("Numeros Sorteados:" );
+        var stopWatch = new Stopwatch();
+
+       
+
+       
 
         for (int i = 0; i < (numeroSorteado.Length); i++)
         {
@@ -21,13 +27,62 @@ class Program
         }
 
 
+        stopWatch.Start();
+        
+        Thread t1 = new Thread(Thread1);
+        t1.Start();
+        t1.Join();
+        Thread t2 = new Thread(Thread2);
+        t2.Start();
+
+        t2.Join();
+
+        somaTotal = soma1 + soma2;
+        Console.WriteLine("\nValor das duas Threads somadas:" + somaTotal);
+
+        stopWatch.Stop();
+
+        Console.WriteLine($"O Tempo de processamento total é de {stopWatch.ElapsedMilliseconds}ms");
+
+
     }
-    private void Thread1()
+    private static void Thread1()
     {
+        PularLinha();
+
+        Console.WriteLine("Numeros Sorteados Thread 1:");
+
         for (int i = 0; i < numeroSorteado.Length / 2; i++)
         {
-           Console.WriteLine(numeroSorteado[i] + " ");
+           Console.Write(numeroSorteado[i] + " - ");
+            soma1 += numeroSorteado[i];
 
         }
+
+        PularLinha();
+        Console.WriteLine("\nSoma dos valores da  Thread 1: " + soma1);
+        
+    }
+    private static void Thread2()
+    {
+        PularLinha();
+
+        Console.WriteLine("Numeros Sorteados Thread 2:");
+
+        for (int i = (numeroSorteado.Length)/2; i <numeroSorteado.Length; i++)
+        {
+            Console.Write(numeroSorteado[i] + " - ");
+            soma2 += numeroSorteado[i];
+
+        }
+
+        PularLinha();
+        Console.WriteLine("\nSoma Thread 2: " + soma2);
+        
+    }
+
+    public static void PularLinha()
+    {
+        Console.WriteLine(" ");
     }
 }
