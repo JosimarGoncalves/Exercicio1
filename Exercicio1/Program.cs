73fed8dfc4bbadd73bd576a7c88aa95a3d47f1eb
Josimar Goncalves <foxchanger@hotmail.com>
2022-03-27 00:34:24 -0300

    Upd Code

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    public static int[] numeroSorteado = new int[100];
+    public static int valorMinimo = 10;
+    public static int valorMaximo = 100;
+    public static int somaA = 0;
+    public static int somaB = 0;
+
+    static void Main()
+    {
+        Random numeroAleatorio = new Random();
+
+        Console.WriteLine("Numeros Sorteados:" );
+
+        for (int i = 0; i < (numeroSorteado.Length); i++)
+        {
+            numeroSorteado[i] = numeroAleatorio.Next(valorMinimo, valorMaximo);
+        }
+
+
+    }
+    private void Thread1()
+    {
+        for (int i = 0; i < numeroSorteado.Length / 2; i++)
+        {
+           Console.WriteLine(numeroSorteado[i] + " ");
+
+        }
+    }
+}
